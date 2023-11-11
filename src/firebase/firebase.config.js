5d8ff2c8342ac5064e31cda3bab07e4ceed2f338
Uzzal Bhowmik <ubcreativity111@gmail.com>
2023-11-12 00:45:48 +0600

    firebase config updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAS15-m0eyocEBVADfGyxXZwUSqSONu2mg",
-  authDomain: "doc-house-454c9.firebaseapp.com",
-  projectId: "doc-house-454c9",
-  storageBucket: "doc-house-454c9.appspot.com",
-  messagingSenderId: "129132217473",
-  appId: "1:129132217473:web:f20ac0df0f307e41f1b980",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
